dfc678d70cd6577cf5003e28a4d819c86638d0b8
Ronchi Miong <miongronchiit1a@gmail.com>
2019-01-22 14:57:24 +0800

    Lorem Update

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -4,7 +4,7 @@
 
 <h1>About Me:</h1>
 <p>
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+     Lorem ipsum dolor sit amet consectetur adipisicing elit. 
     Omnis, dicta a repellendus labore eum voluptatibus ex, 
     adipisci ut impedit corrupti natus sequi, hic ratione laboriosam. 
     Ipsam impedit nam quaerat incidunt.
@@ -12,6 +12,13 @@
     Ratione excepturi illo libero, iure accusantium corrupti. 
     Vel exercitationem blanditiis quasi excepturi beatae ipsa aperiam, 
     saepe nisi impedit? Nisi necessitatibus animi asperiores.
+    Deserunt anim mollit duis excepteur qui culpa laboris eu est do irure. 
+    Labore ut irure irure non dolor esse cupidatat et adipisicing aute non non.
+    Qui duis esse commodo dolore. In nulla anim culpa consequat ullamco nulla mollit proident ea. 
+    Minim non amet consequat irure proident elit in occaecat consequat est non mollit non. 
+    Irure irure cupidatat commodo id do ad nulla do.
+   
+    
 </p> 
 
 @endsection
